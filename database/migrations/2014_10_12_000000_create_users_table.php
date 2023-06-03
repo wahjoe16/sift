916f521a50c2f1b722b0bd7811e8ca90b89fdf32
wahjoe16 <wahjoe16@gmail.com>
2023-06-03 22:13:40 +0700

    migrasi tabel user (admin, dosen, mahasiswa), membuat helper tanggal indonesia

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,25 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('nik', 15);
+            $table->string('nama', 100);
+            $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('level')->default(0);
+            $table->string('telepon', 15)->nullable();
+            $table->string('foto', 100)->nullable();
+            $table->string('jabatan', 100)->nullable();
+            $table->enum('program_studi', [
+                'Teknik Pertambangan',
+                'Perencanaan Wilayah dan Kota',
+                'Teknik Industri'
+            ])->nullable();
+            $table->enum('tipe_dosen', ['internal', 'eksternal'])->nullable();
+            $table->tinyInteger('status_koordinator_skripsi')->default(0);
+            $table->tinyInteger('status_dekanat')->default(0);
+            $table->tinyInteger('status_kaprodi')->default(0);
+            $table->tinyInteger('status_sekprodi')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
