2a66f1f1c98ea6af8e372de7a81db0ca20e2ab43
wahjoe16 <wahjoe16@gmail.com>
2023-06-03 20:51:47 +0700

    install aplikasi sistem informasi fakultas teknik laravel 9, integrasi template adminLTE 2

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,84 @@
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+            </div>
+            <div class="pull-left info">
+                <p>Alexander Pierce</p>
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+        </div>
+        <!-- search form -->
+        <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="Search...">
+                <span class="input-group-btn">
+                    <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                    </button>
+                </span>
+            </div>
+        </form>
+        <!-- /.search form -->
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="header">Main Menu</li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-home"></i> <span>Dashboard</span>
+                </a>
+            </li>
+            <li class="header">Data Master</li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-street-view"></i> <span>Admin</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-user"></i> <span>Dosen</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-users"></i> <span>Mahasiswa</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-hourglass"></i> <span>Tahun Ajaran</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-hourglass-end"></i> <span>Semester</span>
+                </a>
+            </li>
+            <li class="header">Seminar</li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-upload"></i> <span>Pengajuan</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa  fa-file-text"></i> <span>Rekapitulasi</span>
+                </a>
+            </li>
+            <li class="header">Sidang</li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-upload"></i> <span>Pengajuan</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa  fa-file-text"></i> <span>Rekapitulasi</span>
+                </a>
+            </li>
+        </ul>
+    </section>
+    <!-- /.sidebar -->
+</aside>
